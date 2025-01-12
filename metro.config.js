30cfcb776a3ff3789b0b607dbd2041602e4d5cc4
NguyenThanhTai <thanhtai5223@gmail.com>
2025-01-13 02:34:07 +0700

    connect-device-mobile

---
+++ metro.config.js
@@ -0,0 +1,11 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+module.exports = {
+  ...defaultConfig,
+  resolver: {
+    ...defaultConfig.resolver,
+    blockList: [/node_modules\/.*\/node_modules\/expo\//],
+  },
+};
